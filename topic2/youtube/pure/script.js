b3b6ebfc2408fde25fb49459c7e6162d62ce0054
Fernando <prieto.fernando.andres@gmail.com>
2013-10-09 17:23:39 -0300

    commit 10

---
+++ topic2/youtube/pure/script.js
@@ -0,0 +1,13 @@
+$('document').ready(function(){	
+
+	$('#search-form').submit(function(){
+		if($('#search-input').val() != "")
+		{
+			alert("Submit simulation. Searching: "+$('#search-input').val());
+		}
+		else
+		{
+			return false;
+		}
+	});
+});
