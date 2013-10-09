a87d1f17f83a33449cb6dc1f3ec05023451c99b2
Fernando <prieto.fernando.andres@gmail.com>
2013-10-09 13:31:52 -0300

    commit 9

--- topic2/youtube/foundation/js/script.js
+++ topic2/youtube/foundation/js/script.js
@@ -1,14 +1,5 @@
 $('document').ready(function(){
 
-	$('#search-input').focus(function(){
-		$('#search-link').addClass('search-link-input');	
-	});
-
-	$('#search-input').blur(function(){
-		$('#search-link').removeClass('search-link-input');	
-	});
-	
-
 	$('#search-form').submit(function(){
 		if($('#search-input').val() != "")
 		{
